84370f4d923c04c69221a5239c768506f3a22c74
poowadon.git <poowadon.ingeni@gmail.com>
2022-03-01 14:33:34 +0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+
+const app = express();
+app.use(json());
+
+app.all('*', async (req, res) => {
+    return res.send({"hello": "hello"})
+   // throw new NotFoundError();
+  });
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!!");
+  });
